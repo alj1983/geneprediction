3f65ba75e5f27eecefbfb49d8ad2a3704455c1b1
Alexander Jueterbock <Alexander-Jueterbock@web.de>
2015-10-20 21:37:04 +0200

    Included all functions that I adjusted to the Guppy databas into the ProeinScreen module file.

--- TestScript.pl
+++ TestScript.pl
@@ -5,9 +5,9 @@ use ProteinScreen;
 
 
 
-#ProteinScreen::ProteinScreen("/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/Fishes_reproduction_embl.fasta", "/home/alj/Dropbox.personal/Dropbox/Pfam/Pfam-A.hmm", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/nonredundant_dbv1.fasta", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/nr_bonyfishes", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/GuppyESTs.fasta");
+ProteinScreen::ProteinScreen("/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/Fishes_reproduction_embl.fasta", "/home/alj/Dropbox.personal/Dropbox/Pfam/Pfam-A.hmm", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/nonredundant_dbv1.fasta", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/nr_bonyfishes", "/home/alj/Dropbox.personal/Dropbox/Programming/2014CopepodHSPs/201408ProgramAdjustable/TestFiles/GuppyESTs.fasta");
 
 #ProteinScreen::parse_local_database_search("tblastn.LocalDatabase");
 
-ProteinScreen::best_hits_to_fasta("Cfin.compinfo");
+#ProteinScreen::best_hits_to_fasta("Cfin.compinfo");
 #best_hits_to_fasta("Cfin.compinfo");
